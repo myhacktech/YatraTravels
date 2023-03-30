13d7e975fff31400518f79182a8b56ce7d193eec
VAIBHAV SRIVASTAVA <86517958+myhacktech@users.noreply.github.com>
2023-03-30 10:07:12 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  alert("|Welcome to Vikramaditya Travels|\n|happy journey|");
+}
+function my() {
+  var x = document.getElementById("name");
+  x.value = x.value.toUpperCase();
+}
---
+++ index.php
@@ -0,0 +1,299 @@
+<html>
+<head><title>Yatra Travels</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Gemunu+Libre:wght@700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="login.css">
+  <script src=home.js></script>
+</head>
+<style>
+a:visited,a:active,a:hover,a:link {
+	color:white;
+	text-decoration:none;
+	}
+.contact{
+position:absolute;
+top:4900px;
+
+
+}
+.car
+	{position:absolute;
+	top:4400px;
+	
+	height:450px;
+	width:1345px;
+
+
+	background-image:linear-gradient(45deg,purple,orange);
+  
+}
+.im{
+background-color:none;
+position:absolute;
+top:1550px;
+}
+.im:hover{
+background-color:purple;
+
+}
+.bt{
+position:absolute;
+top:2300px;
+width:100px;
+}
+.bt:hover{
+background-color:yellow;
+}
+.login{
+position:absolute;
+
+top:2390px;
+height:390px;
+width:1345px;
+  background:#000 url('pic.jpg') no-repeat center center/cover; 
+}
+body{
+margin:0px;
+
+}
+.color-overlay{
+width:100%;
+height:100%;
+background:black;
+opacity:.2; 
+position:absolute;
+}
+.container{
+
+  width:1347px;
+  height:600px;
+
+  background:#000 url('1.jpg') no-repeat center center/cover; 
+  color:purple;
+  display:flex;
+
+    
+	
+	 position:absolute;
+	 top:0px;
+
+}
+.color-overlay2{
+width:100%;
+height:100%;
+background:black;
+opacity:.2; 
+position:absolute;
+}
+.container2{
+
+  width:1340px;
+  height:550px;
+
+  background:#000 url('we.jpg') no-repeat center center/cover; 
+  color:purple;
+  display:flex;
+
+    
+	
+	 position:absolute;
+	 top:3400px;
+	 
+
+}
+   .blink {
+        animation: blinker 0.6s linear infinite;
+        color:black;
+        font-size: 30px;
+        font-weight: bold;
+        font-family: sans-serif;
+      }
+      @keyframes blinker {
+        50% {
+          opacity: 0;
+        }
+        .last{
+          position: absolute;
+          top:5000px;
+        background-color:#18deec0c;
+        }
+	
+	
+	
+	
+</style>
+<body onload="myFunction()">
+
+<marquee><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<img src=limited.png style="top:100px;z-index:1;height:100px;width:200px;"></marquee>
+<img src="se.png" style="z-index:1;position:absolute;top:200px;left:900px;height:250px;width:250px;"><div class=container>
+<h1 style="color:white;font-size:50px;z-index:1;font-family: 'Ephesis', cursive;top:0px;position:relative;left:260px"><a id=home>Yatra Travels</a></h1>
+<h1 style="position:relative;left:450px;font-size:25px;color:white;z-index:1;top:30px;"><a href=#home>Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=#products>Products</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=#explore>Explore</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=#blog>Blogs</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=#contact>Contact</a></h1>
+<h1 style="font-size:50px;z-index:1;color:white;position:absolute;top:200px;left:150px;font-family: arial;">Come And </h1>
+<h1 style="font-size:50px;z-index:1;color:white;position:absolute;top:250px;left:150px;font-family: arial;">Realise Your Dreams!!! </h1>
+<h1 style="font-size:15px;z-index:1;color:white;position:absolute;top:330px;left:150px;font-family: arial;">One place to find all your destinations and hotels room bookings </h1>
+<h2 style="background-color:yellow;border:2px;border-color:black;font-size:25px;z-index:1;color:black;position:absolute;top:370px;left:150px;font-family: arial;padding:15px;border-radius:4px;">Learn More</h1>
+<h2 style="background-color:green;border:2px;border-color:black;font-size:25px;z-index:1;color:white;position:absolute;top:370px;left:320px;font-family: arial;length:250px;padding:15px;border-radius:4px;">Explore More</h1>
+
+<div class="color-overlay"></div> </div>
+<div style="position:absolute;top:640px;left:420px;font-size:60px;font-family: 'Gemunu Libre', sans-serif;"><b>Welcome to Yatra Travels</b></div>
+
+<div style="font-size:30px;font-family: 'Amatic SC', cursive;position:absolute;top:750px;left:220px;width:450px;height:400px;color:purple;">Sometimes it's not an easy task to find the right place for you.But we here at Yatra travels Let you find one of the best places across the globe that also complies wih your budget.So get ready for an amazng experience ahead with yatra travels.Wish you a Happy Journey</div>
+<img src=13.png style="position:absolute;top:750px;left:1010px;height:250px;width:300px;display:flex;">
+<img src=bell.png style="position:absolute;top:1050px;left:220px;height:100px;width:100px">
+
+<img src=blog.png style="position:absolute;top:1050px;left:650px;height:100px;width:100px">
+
+<img src=contact.png style="position:absolute;top:1050px;left:1040px;height:100px;width:100px">
+<div style="position:absolute;top:1140px;left:180px;font-size:35px;"><b>Notifications</b></div>
+<div style="position:absolute;top:1140px;left:600px;font-size:35px;"><b>Testimonials</b></div>
+<div style="position:absolute;top:1140px;left:1030px;font-size:35px;"><b>Reach Us</b></div>
+<DIV STYLE="position:absolute;top:1190px;left:170px;width:150px;font-size:20px;text-align:center;color:purple;border:groove;border-radius:10px;border-size:3px;border-color:black;padding:20px;">Get Notified on every offer,And hotels discounts. Not only that, give refferals to yours friends,family members,and collgeues.</div>
+<DIV STYLE="position:absolute;top:1190px;left:600px;width:150px;font-size:20px;text-align:center;color:purple;border:groove;border-radius:10px;border-size:3px;border-color:black;padding:20px;">Get Notified on every offer,And hotels discounts. Not only that, give refferals to yours friends,family members,and collgeues.</div>
+
+<DIV STYLE="position:absolute;top:1190px;left:1000px;width:150px;font-size:20px;text-align:center;color:purple;border:groove;border-radius:10px;border-size:3px;border-color:black;padding:20px;">Get Notified on every offer,And hotels discounts. Not only that, give refferals to yours friends,family members,and collgeues.</div>
+<div style="position:absolute;top:1470px;left:550px;font-size:45px;"><b><a id=products>Check Hotels</a></b></div>
+<div  class=im style="left:0px;height:500px;width:330px;border:solid;border-size:1px;"><img src="h2.jpg" style="position:relative;top:0px;left:0px;height:400px;width:330px;">
+<b style="top:380px;left:50px;font-size:35px;font-family: 'Ephesis', cursive;">Hotel surya</b><br>
+<div style="left:30px;font-size:20px;align:center;top:400px;height:20px;width:100px;background-color:orange;border:groove;border-radius:5px;border-color:black;padding:8px;font-family: 'Gemunu Libre', sans-serif;"><b>BOOK NOW<img src=gift.png style="position:absolute;top:410px;right:5px;height:90px;width:90px;"></div>
+</div>
+<div class=im style="left:330px;height:500px;width:330px;border:solid;border-size:1px;"><img src="h1.jpg" style="position:relative;top:0px;left:0px;height:400px;width:330px;">
+<b style="top:380px;left:50px;font-size:35px;font-family: 'Ephesis', cursive;">Taj Hotel</b><br>
+<div style="left:30px;font-size:20px;align:center;top:400px;height:20px;width:100px;background-color:orange;border:groove;border-radius:5px;border-color:black;padding:8px;font-family: 'Gemunu Libre', sans-serif;"><b>BOOK NOW<img src=gift.png style="position:absolute;top:410px;right:5px;height:90px;width:90px;"></div>
+</div>
+<div  class=im style="left:660px;height:500px;width:330px;border:solid;border-size:0px;"><img src="h3.jpg" style="position:relative;top:0px;left:0px;height:400px;width:330px;">
+<b style="top:380px;left:50px;font-size:35px;font-family: 'Ephesis', cursive;">Kesari hotel</b><br>
+<div style="left:30px;font-size:20px;align:center;top:400px;height:20px;width:100px;background-color:orange;border:groove;border-radius:5px;border-color:black;padding:8px;font-family: 'Gemunu Libre', sans-serif;"><b>BOOK NOW<img src=gift.png style="position:absolute;top:410px;right:5px;height:90px;width:90px;"></div>
+</div>
+<div class=im style="left:990px;height:500px;width:335px;border:solid;border-size:0px;"><img src="h4.jpg" style="position:relative;top:0px;left:0px;height:400px;width:335px;">
+<b style="top:380px;left:50px;font-size:35px;font-family: 'Ephesis', cursive;">Bhagwat Hotel</b><br>
+<div style="left:30px;font-size:20px;align:center;top:400px;height:20px;width:100px;background-color:orange;border:groove;border-radius:5px;border-color:black;padding:8px;font-family: 'Gemunu Libre', sans-serif;"><b>BOOK NOW<img src=gift.png style="position:absolute;top:410px;right:5px;height:90px;width:90px;"></div>
+</div>
+
+
+<div style="position:absolute;top:2080px;left:580px;font-family: 'Gemunu Libre', sans-serif;font-size:45px;">Pricing Table</div>
+<hr width=0.4% size=130px color=grey style="position:absolute;top:2200px;left:500px;">
+<hr width=0.4% size=130px color=grey style="position:absolute;top:2200px;left:850px;">
+<div style="position:absolute;top:2160px;left:270px;font-size:30px;color:grey;opacity:.9;">Standard Plan<br><br><div style="font-size:20px;opacity:0.5">FREE<br></div><div style="font-size:20px;opacity:0.8;align-items:center;">1GB storage<br>0 Refferals</div><br>&nbsp;</div><div class=bt style="left:270px;border-radius:5px;border:solid;background-color:none;padding:10px;font-color:white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BUY</div>
+<div style="position:absolute;top:2160px;left:590px;font-size:30px;color:grey;opacity:.9;">Buissness Plan<br><br><div style="font-size:20px;opacity:0.5">Rs.499/month*<br></div><div style="font-size:20px;opacity:0.8">4GB storage<br>5 Refferals</div></div><div class=bt style="left:580px;border-radius:5px;border:solid;background-color:none;padding:10px;font-color:white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BUY</div>
+
+<div style="position:absolute;top:2160px;left:900px;font-size:30px;color:grey;opacity:.9;">Executive Plan<br><br><div style="font-size:20px;opacity:0.5">Rs.899/month*<br></div><div style="font-size:20px;opacity:0.8">10GB storage<br>10 Refferals</div></div><div  class=bt style="left:900px;border-radius:5px;border:solid;background-color:none;padding:10px;font-color:white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BUY</div>
+<div style="position:absolute;top:4400px;width:1345px;height:385px;background-color:none;opacity:1;"></div>
+<iframe style="border:double;border-color:black;z-index:1;position:absolute;top:4440px;left:200px;"width="500" height="320" src="video.mp4">
+</iframe>
+<iframe style="border:double;border-color:black;z-index:1;position:absolute;top:4440px;left:700px;"width="500" height="320" src="tour.mp4">
+</iframe>
+<div class="car"></div>
+
+<br><hr style="position:absolute;top:2780px;" size=8px width=1345px color=black><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
+
+
+<br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<marquee><div style="top:2820px;font-size:35px;font-family: 'Gemunu Libre', sans-serif;"><div class=blink><a id=explore>Meet the Creators</a></div></div></marquee>
+<hr style="z-index:1;position:absolute;top:2370px;" size=6px width=1345px color=black>
+<img src="my.jpg" style="position:absolute;top:2900px;left:350px;border:double;border-radius:100px;height:150px;width:150px;">
+<img src="boy.png" style="position:absolute;top:2900px;left:650px;border:double;border-radius:100px;height:150px;width:150px;">
+<img src="13.png" style="position:absolute;top:2900px;left:900px;border:double;border-radius:100px;height:150px;width:150px;">
+
+<img src="cont.png" style="position:absolute;top:3140px;left:350px;border:double;border-radius:100px;height:150px;width:150px;">
+<img src="se.png" style="position:absolute;top:3140px;left:650px;border:double;border-radius:100px;height:150px;width:150px;">
+<img src="contact.png" style="position:absolute;top:3140px;left:900px;border:double;border-radius:100px;height:150px;width:150px;">
+<div class=container2>
+
+<div class="color-overlay2">
+</div> </div><input type=text placeholder="Enter Your Email To Get Notfied" style="border:groove;border-color:orange;position:absolute;top:3500px;left:350px;height:50px;width:500px;"><div style="padding-top:12px;position:absolute;top:3500px;left:880px;color:orange;background-color:none;border-color:orange;border-radius:4px;width:80px;height:32px;font-family: 'Gemunu Libre', sans-serif;border:groove;border-color:orange;padding-left:13px;">SIGN IN</div>
+<img src=or.png style="background-color:orange;border-radius:50px;position:absolute;top:3600px;height:100px;left:600px;">
+<div style="position:absolute;top:3800px;left:570px;background-color:none;border:groove;border-color:orange;color:orange;font-size:35px;padding:7px;">SIGN UP</div>
+<div style="position:absolute;top:4000px;left:620px;font-family: 'Gemunu Libre', sans-serif;font-size:35px;"><a id=blog>BLOGS</a></div>
+<div style="position:absolute;top:4050px;left:200px;height:400px;width:220px;"><img src=h3.jpg style="top:0px;height:150px;width:220px;">
+<br><div style="top:260px;left:50px;width:210px;;height;100px;border-color:black;border:solid;font-size:30px;padding-top:15px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Browse</div>
+</div>
+<div style="position:absolute;top:4050px;left:550px;height:400px;width:220px;"><img src=h3.jpg style="top:0px;height:150px;width:220px;">
+<br><div style="top:260px;left:50px;width:210px;;height;100px;border-color:black;border:solid;font-size:30px;padding-top:15px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Browse</div>
+</div>
+<div style="position:absolute;top:4050px;left:900px;height:400px;width:220px;"><img src=h3.jpg style="top:0px;height:150px;width:220px;">
+<br><div style="top:260px;left:50px;width:210px;;height;100px;border-color:black;border:solid;font-size:30px;padding-top:8px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Browse</div>
+</div>
+<div class=login >
+
+  <div class="main_div" style="position:absolute;left:900px;">
+    <div class="title">Login Form</div>
+   
+    <form>
+      <div class="input_box">
+       <input id=name onfocusout="my()" type="text" placeholder="Email or Phone" required>
+     
+      </div>
+      <div class="input_box">
+<input type="password" placeholder="Password" required>
+      
+      </div>
+      <div class="option_div">
+        <div class="check_box">
+          <input type="checkbox">
+          <span>Remember me</span>
+        </div>
+      
+          
+        
+      </div>
+      <div class="input_box button">
+        <button   style="background-color:purple;color:white;height:50px;width:370px;font-size:30px;" onclick="alert('Thankyou for Registering\nHave a nice day!!!')">Login</button>
+      </div>
+      
+     </form>
+    </div>
+  </div>
+
+  <div class=contact>
+   <div class="main_div" style="position:absolute;left:900px;">
+    <div class="title">  <a id =contact>Contact Us</a></div>
+   
+    <form>
+      <div class="input_box">
+       <input type="text" placeholder="Email or Phone" required>
+     
+      </div>
+      <div class="input_box">
+<input type="text" placeholder="Say Something" required>
+      
+      </div>
+	   <div class="input_box">
+<input type="text" placeholder="Rate Us(out of 5)" required>
+      
+      </div>
+     
+      <div class="input_box button">
+        <button   style="background-color:purple;color:white;height:50px;width:370px;font-size:30px;" onclick="alert('Thanks for valuable feedback')">Send Us</button>
+      </div>
+     
+     </form>
+    </div>
+  </div>
+<img src=cont.png style="z-index:1;position:absolute;top:4900px;height:300px;width:300px;left:240px;"> <a href=#home>
+<img src=hom.jpg style="z-index:1;position:fixed;top:30px;height:100px;width:100px;left:40px;border-radius:50px;"> </a>
+
+<img src=diwali.png style="z-index:1;position:absolute;top:2420px;height:300px;width:300px;left:240px;"> 
+
+<div style="position:absolute;top:5300px;height:300px;width:1345px;background-image:linear-gradient(35deg,pink,yellow);">
+<div style="position:absolute;top:40px;left:250px;align-items:center;"><b><h3>Official Address</h3></b></div><div style="opacity:.5;position:absolute;top:95px;left:260px;">Sonebhdra City<br>231216<br>Uttar Pradesh</div>
+<div style="position:absolute;top:40px;left:620px;align-items:center;"><b><h3>Follow Us</h3></b></div><img src=social.png style="position:absolute;top:70px;left:500px;height:200px;width:300px;">
+<div style="position:absolute;top:40px;left:920px;align-items:center;"><b><h2>Contact Us</h2></b><br>6357984213<br>7895243568</div>
+
+</div>
